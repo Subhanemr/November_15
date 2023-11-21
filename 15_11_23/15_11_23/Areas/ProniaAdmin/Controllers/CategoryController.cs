7322098b7bbfc8f955418daf1efab36617eb4551
T x <emraslanovsupan290@gmail.com>
2023-11-21 17:22:10 +0400

    Added Category controller and view

---
+++ 15_11_23/15_11_23/Areas/ProniaAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _15_11_23.Areas.ProniaAdmin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
