ade8755b5bfc89aec2f0222200ce4a712f995447
T x <emraslanovsupan290@gmail.com>
2023-11-21 16:41:02 +0400

    Added HomeController and Home View

---
+++ 15_11_23/15_11_23/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _15_11_23.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
