228d2d329a24d9c34f1ede418fd59900ce343735
T x <emraslanovsupan290@gmail.com>
2023-11-22 19:51:00 +0400

    Added Slide More And All mores back buttons

--- 15_11_23/15_11_23/Areas/ProniaAdmin/Controllers/SlideController.cs
+++ 15_11_23/15_11_23/Areas/ProniaAdmin/Controllers/SlideController.cs
@@ -39,7 +39,7 @@ namespace _15_11_23.Areas.ProniaAdmin.Controllers
                 return View();
             }
 
-            if(slide.Photo.Length > 3 * 1024 * 1024)
+            if(slide.Photo.Length > 10 * 1024 * 1024)
             {
                 ModelState.AddModelError("Photo", "Image should not be larger than 10 mb");
                 return View();
@@ -57,6 +57,67 @@ namespace _15_11_23.Areas.ProniaAdmin.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Slide slide = await _context.Slides.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (slide == null) return NotFound();
+
+            return View(slide);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Slide slide)
+        {
+            if (!ModelState.IsValid) return View();
+
+            Slide existed = await _context.Slides.FirstOrDefaultAsync(c => c.Id == id);
+            if (existed == null) return NotFound();
+
+            bool result = _context.Slides
+                .Any(c => c.Title.ToLower().Trim() == slide.Title.ToLower().Trim() && c.Id != id);
+
+            if (result)
+            {
+                ModelState.AddModelError("Name", "A Tag is available");
+                return View();
+            }
+
+            if (slide.Photo is null)
+            {
+                ModelState.AddModelError("Photo", "The image must be uploaded");
+                return View();
+            }
+
+            if (!slide.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "File Not supported");
+                return View();
+            }
+
+            if (slide.Photo.Length > 10 * 1024 * 1024)
+            {
+                ModelState.AddModelError("Photo", "Image should not be larger than 10 mb");
+                return View();
+            }
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            using (FileStream fs = new($@"{currentDirectory}\wwwroot\assets\images\website-images\{slide.Photo.FileName}", FileMode.Create))
+            {
+                await slide.Photo.CopyToAsync(fs);
+            };
+
+            existed.Title = slide.Title;
+            existed.SubTitle = slide.SubTitle;
+            existed.Description = slide.Description;
+            existed.ImgUrl = slide.Photo.FileName;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         public async Task<IActionResult> Delete(int id)
         {
             if (id <= 0) return BadRequest();
@@ -69,6 +130,15 @@ namespace _15_11_23.Areas.ProniaAdmin.Controllers
             await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> More(int id)
+        {
+            if (id <= 0) return BadRequest();
+            Slide slide = await _context.Slides.FirstOrDefaultAsync(c => c.Id == id);
+            if (slide == null) return NotFound();
+
+            return View(slide);
         }
     }
 }
