e2465e2d10613118db2651f63df5c93cc71acf0a
T x <emraslanovsupan290@gmail.com>
2023-11-22 19:04:10 +0400

    Added Into Slide Delete, Create

--- 15_11_23/15_11_23/Areas/ProniaAdmin/Controllers/SlideController.cs
+++ 15_11_23/15_11_23/Areas/ProniaAdmin/Controllers/SlideController.cs
@@ -24,5 +24,51 @@ namespace _15_11_23.Areas.ProniaAdmin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(Slide slide)
+        {
+            if(slide.Photo is null)
+            {
+                ModelState.AddModelError("Photo", "The image must be uploaded");
+                return View();
+            }
+
+            if (!slide.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "File Not supported");
+                return View();
+            }
+
+            if(slide.Photo.Length > 3 * 1024 * 1024)
+            {
+                ModelState.AddModelError("Photo", "Image should not be larger than 10 mb");
+                return View();
+            }
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            using(FileStream fs = new  ($@"{currentDirectory}\wwwroot\assets\images\website-images\{slide.Photo.FileName}", FileMode.Create))
+            {
+                await slide.Photo.CopyToAsync(fs);
+            };
+
+            slide.ImgUrl = slide.Photo.FileName;
+
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            Slide existed = await _context.Slides.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (existed == null) return NotFound();
+
+            _context.Slides.Remove(existed);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- 15_11_23/15_11_23/Models/Slide.cs
+++ 15_11_23/15_11_23/Models/Slide.cs
@@ -1,12 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _15_11_23.Models
 {
     public class Slide
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name must be entered mutled")]
+        [MaxLength(25, ErrorMessage = "It should not exceed 25 characters")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Name must be entered mutled")]
+        [MaxLength(50, ErrorMessage = "It should not exceed 25 characters")]
         public string SubTitle { get; set; }
+        [Required(ErrorMessage = "Name must be entered mutled")]
+        [MaxLength(100, ErrorMessage = "It should not exceed 25 characters")]
         public string Description { get; set; }
         public string ImgUrl { get; set; }
         [NotMapped]
