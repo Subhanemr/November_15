081ad5aa881026501718f9b78c249921303f2492
T x <emraslanovsupan290@gmail.com>
2023-11-23 20:59:48 +0400

    Validation message Fixed

--- 15_11_23/15_11_23/Models/Slide.cs
+++ 15_11_23/15_11_23/Models/Slide.cs
@@ -10,10 +10,10 @@ namespace _15_11_23.Models
         [MaxLength(25, ErrorMessage = "It should not exceed 25 characters")]
         public string Title { get; set; }
         [Required(ErrorMessage = "Name must be entered mutled")]
-        [MaxLength(50, ErrorMessage = "It should not exceed 25 characters")]
+        [MaxLength(50, ErrorMessage = "It should not exceed 50 characters")]
         public string SubTitle { get; set; }
         [Required(ErrorMessage = "Name must be entered mutled")]
-        [MaxLength(100, ErrorMessage = "It should not exceed 25 characters")]
+        [MaxLength(100, ErrorMessage = "It should not exceed 100 characters")]
         public string Description { get; set; }
         public string? ImgUrl { get; set; }
         [NotMapped]
