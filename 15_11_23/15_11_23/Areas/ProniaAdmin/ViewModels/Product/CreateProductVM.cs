fa87de125a557f7bcf38426b3b0bcbce6262737f
T x <emraslanovsupan290@gmail.com>
2023-11-26 22:44:11 +0400

    Added Price range

--- 15_11_23/15_11_23/Areas/ProniaAdmin/ViewModels/Product/CreateProductVM.cs
+++ 15_11_23/15_11_23/Areas/ProniaAdmin/ViewModels/Product/CreateProductVM.cs
@@ -9,8 +9,10 @@ namespace _15_11_23.Areas.ProniaAdmin.ViewModels
         [MaxLength(25, ErrorMessage = "It should not exceed 25 characters")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Price must be entered mutled")]
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be greater than 0 ")]
         public decimal Price { get; set; }
         [Required(ErrorMessage = "Order must be entered mutled")]
+        [Range(1, int.MaxValue, ErrorMessage = "Order must be greater than 0 ")]
         public int CountId { get; set; }
         [Required(ErrorMessage = "SKU must be entered mutled")]
         public string SKU { get; set; }
@@ -18,6 +20,8 @@ namespace _15_11_23.Areas.ProniaAdmin.ViewModels
         [MaxLength(100, ErrorMessage = "It should not exceed 25 characters")]
         public string Description { get; set; }
         [Required(ErrorMessage = "Category must be entered mutled")]
+        [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than 0 ")]
+
         public int? CategoryId { get; set; }
     }
 }
