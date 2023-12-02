06263578cc0b8e9db37f380373d4aa3f34da1f8c
T x <emraslanovsupan290@gmail.com>
2023-12-02 17:24:42 +0400

    Added Count Plus and Minus, but changed Desing

--- 15_11_23/15_11_23/Controllers/CartController.cs
+++ 15_11_23/15_11_23/Controllers/CartController.cs
@@ -21,7 +21,7 @@ namespace _15_11_23.Controllers
             List<CartItemVM> cartVM = new List<CartItemVM>();
             if (Request.Cookies["Basket"] is not null)
             {
-                 List<CartCookieItemVM> cart = JsonConvert.DeserializeObject<List<CartCookieItemVM>>(Request.Cookies["Basket"]);
+                List<CartCookieItemVM> cart = JsonConvert.DeserializeObject<List<CartCookieItemVM>>(Request.Cookies["Basket"]);
                 foreach (CartCookieItemVM cartCookieItemVM in cart)
                 {
                     Product product = await _context.Products.Include(p => p.ProductImages.Where(pi => pi.IsPrimary == true)).FirstOrDefaultAsync(p => p.Id == cartCookieItemVM.Id);
@@ -35,7 +35,7 @@ namespace _15_11_23.Controllers
                             Image = product.ProductImages.FirstOrDefault().Url,
                             Count = cartCookieItemVM.Count,
                             SubTotal = Convert.ToDecimal(cartCookieItemVM.Count) * product.Price
-                            
+
                         };
                         cartVM.Add(cartItemVM);
                     }
@@ -74,7 +74,7 @@ namespace _15_11_23.Controllers
             }
             else
             {
-                cart = new  List<CartCookieItemVM>();
+                cart = new List<CartCookieItemVM>();
                 CartCookieItemVM cartCookieItem = new CartCookieItemVM
                 {
                     Id = id,
@@ -88,5 +88,70 @@ namespace _15_11_23.Controllers
 
             return RedirectToAction(nameof(Index), "Home");
         }
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            List<CartCookieItemVM> cart = JsonConvert.DeserializeObject<List<CartCookieItemVM>>(Request.Cookies["Basket"]);
+
+            CartCookieItemVM item = cart.FirstOrDefault(c => c.Id == id);
+
+            if (item == null) return BadRequest();
+
+            cart.Remove(item);
+
+
+            string json = JsonConvert.SerializeObject(cart);
+            Response.Cookies.Append("Basket", json, new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(1)
+            });
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> CountMinus(int id)
+        {
+            List<CartCookieItemVM> cart = JsonConvert.DeserializeObject<List<CartCookieItemVM>>(Request.Cookies["Basket"]);
+
+            CartCookieItemVM item = cart.FirstOrDefault(c => c.Id == id);
+
+            if (item == null) return BadRequest();
+
+            item.Count--;
+            if (item.Count <= 0)
+            {
+                return await DeleteItem(id);
+            }
+
+            string json = JsonConvert.SerializeObject(cart);
+            Response.Cookies.Append("Basket", json, new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(1)
+            });
+
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> CountPlus(int id)
+        {
+            List<CartCookieItemVM> cart = JsonConvert.DeserializeObject<List<CartCookieItemVM>>(Request.Cookies["Basket"]);
+
+            CartCookieItemVM item = cart.FirstOrDefault(c => c.Id == id);
+
+            if (item == null) return BadRequest();
+
+            item.Count++;
+            if (item.Count <= 0)
+            {
+                return await DeleteItem(id);
+            }
+
+            string json = JsonConvert.SerializeObject(cart);
+            Response.Cookies.Append("Basket", json, new CookieOptions
+            {
+                Expires = DateTimeOffset.Now.AddDays(1)
+            });
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
