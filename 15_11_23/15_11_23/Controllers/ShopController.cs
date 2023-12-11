569aa6da6e75f13122ff8a727e0b6b1b63729308
T x <emraslanovsupan290@gmail.com>
2023-12-11 19:57:22 +0400

    Added Filter and Combination

---
+++ 15_11_23/15_11_23/Controllers/ShopController.cs
@@ -0,0 +1,57 @@
+﻿using _15_11_23.DAL;
+using _15_11_23.Models;
+using _15_11_23.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _15_11_23.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        public ShopController(AppDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string? search, int? order, int? categoryId)
+        {
+            IQueryable<Product> queryable = _context.Products.Include(pi => pi.ProductImages.Where(a => a.IsPrimary != null)).AsQueryable();
+            switch (order)
+            {
+                case 1:
+                    queryable = queryable.OrderBy(p => p.Name);
+                    break;
+                case 2:
+                    queryable = queryable.OrderBy(p => p.Price);
+                    break;
+                case 3:
+                    queryable = queryable.OrderByDescending(p => p.Id);
+                    break;
+                case 4:
+                    queryable = queryable.OrderByDescending(p => p.Price);
+                    break;
+            }
+            if (!string.IsNullOrEmpty(search))
+            {
+                queryable = queryable.Where(p => p.Name.ToLower().Contains(search.ToLower()));
+            }
+            if (categoryId != null)
+            {
+                queryable = queryable.Where(p => p.CategoryId == categoryId);
+            }
+            ShopVM shopVM = new ShopVM
+            {
+                Categories = await _context.Categories.Include(c => c.Products).ToListAsync(),
+                Products = await queryable.ToListAsync(),
+                Order = order,
+                Search = search,
+                CategoryId = categoryId,
+            };
+            return View(shopVM);
+        }
+    }
+}
---
+++ 15_11_23/15_11_23/ViewModel/ShopVM.cs
@@ -0,0 +1,14 @@
+﻿using _15_11_23.Models;
+
+namespace _15_11_23.ViewModel
+{
+    public class ShopVM
+    {
+
+        public int? Order { get; set; }
+        public int? CategoryId { get; set; }
+        public string? Search { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
